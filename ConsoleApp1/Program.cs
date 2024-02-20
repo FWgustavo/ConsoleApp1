a333755b70a2278e30d5ff71442c34861c953f6c
Usuario <Usuario@CPD02-M01>
2024-02-20 12:26:48 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeiraAulaDeCsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo!!!");
+            Console.ReadKey();
+
+            Console.WriteLine(7);
+            Console.ReadKey();
+
+            Console.WriteLine(7.7);
+            Console.ReadKey();
+
+            Console.WriteLine("O resultado é: " + (7 + 7 * 7));
+            Console.ReadKey();
+
+            Console.WriteLine(7 + 7 + " é o resultado");
+            Console.ReadKey();
+
+            Console.Beep();
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
